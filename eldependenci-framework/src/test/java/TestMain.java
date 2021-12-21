7bcc043a456ab28cbf508be23c144c1a023b2c27
eric2788 <tsukiko852@gmail.com>
2021-12-21 15:20:25 +0800

    新增類似 Promise.all 的多異步處理; LangController 新增 get(node, o...) 參數以使用 MessageFormat.format

--- eldependenci-framework/src/test/java/TestMain.java
+++ eldependenci-framework/src/test/java/TestMain.java
@@ -1,11 +1,20 @@
+import com.ericlam.mc.eld.services.ScheduleService;
+
+import java.util.List;
+
 public class TestMain {
+
+    private static ScheduleService scheduleService;
+
     public static void main(String[] args) {
-        System.out.println(AAAA.class.isAssignableFrom(AA.class));
+        scheduleService.callAllAsync(null, List.of(
+                scheduleService.callAsync(null, () -> "string"),
+                scheduleService.callAsync(null, () -> 123)
+        )).thenRunAsync(results -> {
+            String string = (String) results[0];
+            int number = (Integer) results[1];
+            //...
+        }).join();
     }
 
-    public static abstract class AAAA {
-    }
-
-    public static class AA extends AAAA {
-    }
 }
