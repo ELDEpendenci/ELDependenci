a05e745bd4e2ade3caf7fc552b38d18da7d03391
eric2788 <tsukiko852@gmail.com>
2022-05-07 23:50:33 +0800

    update javadocs

--- eldependenci-framework/src/main/java/com/ericlam/mc/eld/misc/DebugLogger.java
+++ eldependenci-framework/src/main/java/com/ericlam/mc/eld/misc/DebugLogger.java
@@ -2,26 +2,78 @@ package com.ericlam.mc.eld.misc;
 
 import java.util.logging.Logger;
 
+/**
+ * 可用於 debug 的日誌輸出
+ */
 public interface DebugLogger {
 
+    /**
+     * debug 輸出
+     * @param message 訊息
+     * @param args 訊息參數
+     */
     void debug(String message, Object... args);
 
+    /**
+     * debug 輸出
+     * @param throwable 報錯物件
+     * @param message 訊息
+     * @param args 訊息參數
+     */
     void debug(Throwable throwable, String message, Object... args);
 
+    /**
+     * debug 輸出
+     * @param throwable 報錯物件
+     */
     void debug(Throwable throwable);
 
+    /**
+     * info 輸出
+     * @param message 訊息
+     * @param args 訊息參數
+     */
     void info(String message, Object... args);
 
+    /**
+     * info 輸出
+     * @param throwable 報錯物件
+     * @param message 訊息
+     * @param args 訊息參數
+     */
     void info(Throwable throwable, String message, Object... args);
 
+    /**
+     * info 輸出
+     * @param throwable 報錯物件
+     */
     void info(Throwable throwable);
 
+    /**
+     * warn 輸出
+     * @param message 訊息
+     * @param args 訊息參數
+     */
     void warn(String message, Object... args);
 
+    /**
+     * warn 輸出
+     * @param throwable 報錯物件
+     * @param message 訊息
+     * @param args 訊息參數
+     */
     void warn(Throwable throwable, String message, Object... args);
 
+    /**
+     * warn 輸出
+     * @param throwable 報錯物件
+     */
     void warn(Throwable throwable);
 
+    /**
+     * 獲取真正的 Logger
+     * @return Logger
+     */
     Logger getRealLogger();
 
 }
--- eldependenci-framework/src/main/java/com/ericlam/mc/eld/services/LoggingService.java
+++ eldependenci-framework/src/main/java/com/ericlam/mc/eld/services/LoggingService.java
@@ -4,10 +4,23 @@ package com.ericlam.mc.eld.services;
 import com.ericlam.mc.eld.misc.DebugLogger;
 import org.bukkit.plugin.Plugin;
 
+/**
+ * 日誌輸出服務
+ */
 public interface LoggingService {
 
+    /**
+     * 獲取日誌輸出
+     * @param cls 物件類別
+     * @return debug 日誌輸出
+     */
     DebugLogger getLogger(Class<?> cls);
 
+    /**
+     * 獲取日誌輸出
+     * @param name 日誌名稱
+     * @return debug 日誌輸出
+     */
     DebugLogger getLogger(String name);
 
 
