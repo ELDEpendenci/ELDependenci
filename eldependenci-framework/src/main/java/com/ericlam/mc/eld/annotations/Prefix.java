88006ca5f84f34d45aa1887853f27dbe3a9a5629
eric2788 <tsukiko852@gmail.com>
2022-05-21 00:55:42 +0800

    修復 life cycle 沒有運行, 所有類型標註現在支援繼承類

--- eldependenci-framework/src/main/java/com/ericlam/mc/eld/annotations/Prefix.java
+++ eldependenci-framework/src/main/java/com/ericlam/mc/eld/annotations/Prefix.java
@@ -1,13 +1,11 @@
 package com.ericlam.mc.eld.annotations;
 
-import java.lang.annotation.ElementType;
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
-import java.lang.annotation.Target;
+import java.lang.annotation.*;
 
 /**
  * 前綴，用於訊息文件
  */
+@Inherited
 @Target(ElementType.TYPE)
 @Retention(RetentionPolicy.RUNTIME)
 public @interface Prefix {
