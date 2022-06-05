e5ccceb1b085d07db5dbf22e820511c2c1f6bf91
eric2788 <tsukiko852@gmail.com>
2022-06-05 15:52:09 +0800

    added cache for reflection service

--- eldependenci-common/src/main/java/com/ericlam/mc/eld/services/ELDReflectionService.java
+++ eldependenci-common/src/main/java/com/ericlam/mc/eld/services/ELDReflectionService.java
@@ -1,12 +1,15 @@
 package com.ericlam.mc.eld.services;
 
 import com.google.common.collect.Lists;
+import com.google.common.collect.Maps;
 
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class ELDReflectionService implements ReflectionService {
 
@@ -20,8 +23,32 @@ public class ELDReflectionService implements ReflectionService {
         return getDeclaredMethodsUpToStatic(startClass, exclusiveParent != null ? exclusiveParent : Object.class);
     }
 
+    private static final Map<Class<?>, Field[]> declaredFieldsCache = new HashMap<>();
+    private static final Map<Class<?>, Method[]> declaredMethodsCache = new HashMap<>();
+
+    private static Field[] getDeclareFields(Class<?> cls){
+        if (declaredFieldsCache.containsKey(cls)){
+            return declaredFieldsCache.get(cls);
+        }else{
+            Field[] fields = cls.getDeclaredFields();
+            declaredFieldsCache.put(cls, fields);
+            return fields;
+        }
+    }
+
+    private static Method[] getDeclareMethods(Class<?> cls){
+        if (declaredMethodsCache.containsKey(cls)){
+            return declaredMethodsCache.get(cls);
+        }else{
+            Method[] methods = cls.getDeclaredMethods();
+            declaredMethodsCache.put(cls, methods);
+            return methods;
+        }
+    }
+
+
     public static List<Field> getDeclaredFieldsUpToStatic(@Nonnull Class<?> startClass, @Nullable Class<?> exclusiveParent) {
-        List<Field> currentClassFields = Lists.newArrayList(startClass.getDeclaredFields());
+        List<Field> currentClassFields = Lists.newArrayList(getDeclareFields(startClass));
         Class<?> parentClass = startClass.getSuperclass();
 
         if (parentClass != null && !(parentClass.equals(exclusiveParent))) {
@@ -34,7 +61,7 @@ public class ELDReflectionService implements ReflectionService {
     }
 
     public List<Method> getDeclaredMethodsUpToStatic(@Nonnull Class<?> startClass, @Nullable Class<?> exclusiveParent) {
-        List<Method> currentClassMethods = Lists.newArrayList(startClass.getDeclaredMethods());
+        List<Method> currentClassMethods = Lists.newArrayList(getDeclareMethods(startClass));
         Class<?> parentClass = startClass.getSuperclass();
 
         if (parentClass != null && !(parentClass.equals(exclusiveParent))) {
